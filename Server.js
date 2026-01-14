5e2c73ee843404e94a50b431a1877de11eaf6dee
PEARL <pearlsingh890@gmail.com>
2026-01-14 23:29:36 +0530

    start cmd

--- Server.js
+++ Server.js
@@ -83,5 +83,5 @@ cron.schedule("* * * * *", async () => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port http://localhost:${PORT}`);
 });
